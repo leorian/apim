69ae40b4289542b44d507570e731df28961bf9de
xiezg <xiezg@317hu.com>
2017-06-30 09:19:19 +0800

    api接口文档管理系统

---
+++ src/main/java/com/leorain/apim/enums/UserSexEnum.java
@@ -0,0 +1,5 @@
+package com.leorain.apim.enums;
+
+public enum UserSexEnum {
+	MAN, WOMAN
+}
---
+++ src/main/java/com/leorain/apim/web/UserController.java
@@ -0,0 +1,47 @@
+package com.leorain.apim.web;
+
+import java.util.List;
+
+import com.leorain.apim.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.leorain.apim.mapper.UserMapper;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	@RequestMapping("/getUsers")
+	public List<UserEntity> getUsers() {
+		List<UserEntity> users=userMapper.getAll();
+		return users;
+	}
+	
+    @RequestMapping("/getUser")
+    public UserEntity getUser(Long id) {
+    	UserEntity user=userMapper.getOne(id);
+        return user;
+    }
+    
+    @RequestMapping("/add")
+    public void save(UserEntity user) {
+    	userMapper.insert(user);
+    }
+    
+    @RequestMapping(value="update")
+    public void update(UserEntity user) {
+    	userMapper.update(user);
+    }
+    
+    @RequestMapping(value="/delete/{id}")
+    public void delete(@PathVariable("id") Long id) {
+    	userMapper.delete(id);
+    }
+    
+    
+}
