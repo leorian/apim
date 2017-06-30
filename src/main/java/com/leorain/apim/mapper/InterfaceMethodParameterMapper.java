9b11042a357e21156976343e9ca1193cd341bdc7
xiezg <1573623031@qq.com>
2017-06-30 13:44:02 +0800

    api接口文档管理系统设计文档

---
+++ src/main/java/com/leorain/apim/mapper/InterfaceMethodParameterMapper.java
@@ -0,0 +1,20 @@
+package com.leorain.apim.mapper;
+
+import com.leorain.apim.entity.InterfaceMethodParameterEntity;
+
+import java.util.List;
+
+/**
+ * Created by xiezg@317hu.com on 2017/6/30 0030.
+ */
+public interface InterfaceMethodParameterMapper {
+    List<InterfaceMethodParameterEntity> getAll();
+
+    InterfaceMethodParameterEntity getOne(Long id);
+
+    void insert(InterfaceMethodParameterEntity interfaceMethodParameterEntity);
+
+    void update(InterfaceMethodParameterEntity interfaceMethodParameterEntity);
+
+    void delete(Long id);
+}
