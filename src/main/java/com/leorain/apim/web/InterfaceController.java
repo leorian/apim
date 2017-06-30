b30accb18af9cd25a92a3b58d0a1a1aba370391c
xiezg <1573623031@qq.com>
2017-06-30 15:54:03 +0800

    api接口文档管理系统设计文档

--- src/main/java/com/leorain/apim/web/InterfaceController.java
+++ src/main/java/com/leorain/apim/web/InterfaceController.java
@@ -2,12 +2,14 @@ package com.leorain.apim.web;
 
 import com.leorain.apim.sevice.InterfaceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by xiezg@317hu.com on 2017/6/30 0030.
  */
 @RestController
+@RequestMapping("/interface")
 public class InterfaceController {
     @Autowired
     private InterfaceService interfaceService;
--- src/main/java/com/leorain/apim/web/InterfaceMethodReturnTypeController.java
+++ src/main/java/com/leorain/apim/web/InterfaceMethodReturnTypeController.java
@@ -2,12 +2,14 @@ package com.leorain.apim.web;
 
 import com.leorain.apim.sevice.InterfaceMethodReturnTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by xiezg@317hu.com on 2017/6/30 0030.
  */
 @RestController
+@RequestMapping("/interfaceMethodReturnType")
 public class InterfaceMethodReturnTypeController {
     @Autowired
     private InterfaceMethodReturnTypeService interfaceMethodReturnTypeService;
