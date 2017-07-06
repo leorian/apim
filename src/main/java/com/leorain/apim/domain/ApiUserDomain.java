038e1e69025f36e84c5e5f189c6915deb5b0f959
xiezg <1573623031@qq.com>
2017-07-06 17:11:12 +0800

    api接口文档管理系统设计文档

---
+++ src/main/java/com/leorain/apim/domain/ApiUserDomain.java
@@ -0,0 +1,76 @@
+package com.leorain.apim.domain;
+
+import com.alibaba.fastjson.JSON;
+import com.leorain.apim.entity.ApiUserEntity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by xiezg@317hu.com on 2017/7/6 0006.
+ */
+public class ApiUserDomain {
+    private String usNameInput;
+    private String cnNameInput;
+    private String userRoleInput;
+    private String cellPhoneInput;
+    private String emailInput;
+
+    public String getUsNameInput() {
+        return usNameInput;
+    }
+
+    public void setUsNameInput(String usNameInput) {
+        this.usNameInput = usNameInput;
+    }
+
+    public String getCnNameInput() {
+        return cnNameInput;
+    }
+
+    public void setCnNameInput(String cnNameInput) {
+        this.cnNameInput = cnNameInput;
+    }
+
+    public String getUserRoleInput() {
+        return userRoleInput;
+    }
+
+    public void setUserRoleInput(String userRoleInput) {
+        this.userRoleInput = userRoleInput;
+    }
+
+    public String getCellPhoneInput() {
+        return cellPhoneInput;
+    }
+
+    public void setCellPhoneInput(String cellPhoneInput) {
+        this.cellPhoneInput = cellPhoneInput;
+    }
+
+    public String getEmailInput() {
+        return emailInput;
+    }
+
+    public void setEmailInput(String emailInput) {
+        this.emailInput = emailInput;
+    }
+
+    public ApiUserEntity buildApiUserEntity() {
+        ApiUserEntity apiUserEntity = new ApiUserEntity();
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+        apiUserEntity.setUserId(Long.parseLong(simpleDateFormat.format(date)));
+        apiUserEntity.setUsName(this.getUsNameInput());
+        apiUserEntity.setCnName(this.getCnNameInput());
+        apiUserEntity.setUserRole(Integer.parseInt(this.getUserRoleInput()));
+        apiUserEntity.setCellPhone(this.getCellPhoneInput());
+        apiUserEntity.setEmail(this.getEmailInput());
+        return apiUserEntity;
+    }
+
+    @Override
+    public String toString() {
+        return JSON.toJSONString(this);
+    }
+}
