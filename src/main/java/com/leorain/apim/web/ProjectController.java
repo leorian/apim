dc2b865ba1d6114d345efb040e1477e626bcc85e
xiezg <1573623031@qq.com>
2017-07-07 10:47:14 +0800

    api接口文档管理系统设计文档

--- src/main/java/com/leorain/apim/web/ProjectController.java
+++ src/main/java/com/leorain/apim/web/ProjectController.java
@@ -6,10 +6,12 @@ import com.leorain.apim.entity.ProjectEntity;
 import com.leorain.apim.entity.ProjectMemberEntity;
 import com.leorain.apim.sevice.ProjectMemberService;
 import com.leorain.apim.sevice.ProjectService;
+import com.leorain.apim.tools.JqPage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -45,6 +47,25 @@ public class ProjectController {
     @RequestMapping("/findProjectList")
     public List<ProjectEntity> findProjectList() {
         return projectService.findProjectEntityList();
+    }
+
+    /**
+     * 分页查询项目信息
+     *
+     * @return
+     */
+    @RequestMapping("/findProjectPage")
+    public JqPage<ProjectEntity> findProjectPage(@RequestParam(value = "currentPage", defaultValue = "1") Integer currentPage,
+                                                 @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
+                                                 @RequestParam(value = "projectNameCondition", required = false) String projectNameCondition,
+                                                 @RequestParam(value = "projectManagerCondition", required = false) String projectManagerCondition) {
+        JqPage jqPage = new JqPage();
+        jqPage.setPage(currentPage);
+        jqPage.setPageSize(pageSize);
+        ProjectEntity projectEntity = new ProjectEntity();
+        projectEntity.setProjectName(projectNameCondition);
+        projectEntity.setProjectManager(Long.valueOf(projectManagerCondition));
+        return projectService.findProjectEntityPage(jqPage, projectEntity);
     }
 
 
