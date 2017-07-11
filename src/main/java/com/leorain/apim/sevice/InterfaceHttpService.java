01313253cc193d7e67f9a98730f4dbdaba93c47d
xiezg <1573623031@qq.com>
2017-07-11 11:14:02 +0800

    api接口文档管理系统设计文档

---
+++ src/main/java/com/leorain/apim/sevice/InterfaceHttpService.java
@@ -0,0 +1,7 @@
+package com.leorain.apim.sevice;
+
+/**
+ * Created by xiezg@317hu.com on 2017/7/11 0011.
+ */
+public interface InterfaceHttpService {
+}
---
+++ src/main/java/com/leorain/apim/sevice/impl/InterfaceHttpServiceImpl.java
@@ -0,0 +1,16 @@
+package com.leorain.apim.sevice.impl;
+
+import com.leorain.apim.mapper.InterfaceHttpMapper;
+import com.leorain.apim.sevice.InterfaceHttpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by xiezg@317hu.com on 2017/7/11 0011.
+ */
+@Service
+public class InterfaceHttpServiceImpl implements InterfaceHttpService {
+    @Autowired
+    private InterfaceHttpMapper interfaceHttpMapper;
+
+}
