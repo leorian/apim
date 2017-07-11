1094d4c9402c2844cfe926651db334e496acd0c8
xiezg <1573623031@qq.com>
2017-07-11 11:09:19 +0800

    api接口文档管理系统设计文档

---
+++ src/main/java/com/leorain/apim/mapper/InterfaceHttpMapper.java
@@ -0,0 +1,10 @@
+package com.leorain.apim.mapper;
+
+import com.leorain.apim.entity.InterfaceHttpEntity;
+import com.leorain.apim.mapper.base.BaseMapper;
+
+/**
+ * Created by xiezg@317hu.com on 2017/7/11 0011.
+ */
+public interface InterfaceHttpMapper extends BaseMapper<InterfaceHttpEntity> {
+}
