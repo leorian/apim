7f1b293b6036bd453f8177afa7519cb71132a265
xiezg <1573623031@qq.com>
2017-07-13 09:06:39 +0800

    api接口文档管理系统设计文档

--- src/main/java/com/leorain/apim/web/InterfaceHttpController.java
+++ src/main/java/com/leorain/apim/web/InterfaceHttpController.java
@@ -2,9 +2,19 @@ package com.leorain.apim.web;
 
 import com.alibaba.fastjson.JSON;
 import com.leorain.apim.domain.InterfaceHttpDomain;
+import com.leorain.apim.domain.ResultDomain;
+import com.leorain.apim.entity.InterfaceHttpEntity;
+import com.leorain.apim.entity.InterfaceParamEntity;
+import com.leorain.apim.entity.InterfaceResultEntity;
+import com.leorain.apim.sevice.InterfaceHttpService;
+import com.leorain.apim.sevice.InterfaceParamService;
+import com.leorain.apim.sevice.InterfaceResultService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by xiezg@317hu.com on 2017/7/12 0012.
@@ -13,9 +23,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/interfaceHttp")
 public class InterfaceHttpController {
 
+    @Autowired
+    private InterfaceHttpService interfaceHttpService;
+
+    @Autowired
+    private InterfaceParamService interfaceParamService;
+
+    @Autowired
+    private InterfaceResultService interfaceResultService;
+
+
     @RequestMapping(value = "/insertInterfaceHttp", method = RequestMethod.POST)
-    public String insertInterfaceHttp(InterfaceHttpDomain interfaceHttpDomain) {
+    public ResultDomain insertInterfaceHttp(InterfaceHttpDomain interfaceHttpDomain) {
         System.out.println(JSON.toJSONString(interfaceHttpDomain));
-        return "String";
+        InterfaceHttpEntity interfaceHttpEntity = interfaceHttpDomain.buildInterfaceHttpEntity();
+        interfaceHttpService.insertInterfaceHttpEntity(interfaceHttpEntity);
+        List<InterfaceParamEntity> interfaceParamEntities = interfaceHttpDomain.
+                buildInterfaceParamEntities(interfaceHttpEntity.getId());
+        interfaceParamService.insertInterfaceParamEntities(interfaceParamEntities);
+        List<InterfaceResultEntity> interfaceResultEntities = interfaceHttpDomain.
+                buildInterfaceResultEntities(interfaceHttpEntity.getId());
+        interfaceResultService.insertInterfaceResultEntities(interfaceResultEntities);
+        return new ResultDomain();
     }
 }
