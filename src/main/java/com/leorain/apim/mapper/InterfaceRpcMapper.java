6f98a83c35098841d4f30ea0f90b49d11b8c6e26
xiezhonggui <1573623031@qq.com>
2017-07-15 09:28:41 +0800

    api接口管理平台开发

---
+++ src/main/java/com/leorain/apim/mapper/InterfaceRpcMapper.java
@@ -0,0 +1,10 @@
+package com.leorain.apim.mapper;
+
+import com.leorain.apim.entity.InterfaceRpcEntity;
+import com.leorain.apim.mapper.base.BaseMapper;
+
+/**
+ * Created by xiezhonggui on 2017/7/15.
+ */
+public interface InterfaceRpcMapper extends BaseMapper<InterfaceRpcEntity> {
+}
