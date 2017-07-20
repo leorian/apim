12ecde3b480c5e95879e06938f45975aa0047c82
xiezg <1573623031@qq.com>
2017-07-20 10:34:57 +0800

    api接口文档管理系统设计文档

--- src/main/resources/static/webFrame/handlebars-jquery-plugin.js
+++ src/main/resources/static/webFrame/handlebars-jquery-plugin.js
@@ -1,10 +1,36 @@
-(function($) {
+(function ($) {
     var compiled = {};
-    $.fn.handlebars = function(template, data) {
+
+    function add0(m) {
+        return m < 10 ? '0' + m : m
+    }
+
+    $.fn.handlebars = function (template, data) {
         if (template instanceof jQuery) {
             template = $(template).html();
         }
         compiled[template] = Handlebars.compile(template);
         this.html(compiled[template](data));
     };
+
+    $.format = function (timestamp) {
+        //timestamp是整数，否则要parseInt转换,不会出现少个0的情况
+
+        var time = new Date(timestamp);
+        var year = time.getFullYear();
+        var month = time.getMonth() + 1;
+        var date = time.getDate();
+        var hours = time.getHours();
+        var minutes = time.getMinutes();
+        var seconds = time.getSeconds();
+        return year + '-' + add0(month) + '-' + add0(date) + ' ' + add0(hours) + ':' + add0(minutes) + ':' + add0(seconds);
+    };
+
+    $.getQueryString = function (name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return unescape(r[2]);
+        return null;
+    };
+
 })(jQuery);
